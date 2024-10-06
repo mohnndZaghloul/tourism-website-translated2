b3cb4d2da36a6056dc36a90baedca6dcf05f7260
mohnndZaghloul <mohanndzaghloul17@gmail.com>
2024-10-06 16:33:33 +0300

    fix bugs

--- src/components/templates/Hero.tsx
+++ src/components/templates/Hero.tsx
@@ -1,7 +1,4 @@
-import { Swiper, SwiperSlide } from "swiper/react";
 import hero1 from "../../assets/hero1.jpg";
-import hero2 from "../../assets/hero2.jpg";
-import hero3 from "../../assets/hero3.jpg";
 
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
